a37627b838ebffe19a90e012bc597a07d17423ed
lpremuda <lucas.premuda@gmail.com>
2021-05-23 11:29:16 -0700

    Added || <port_name>

--- server.js
+++ server.js
@@ -6,15 +6,19 @@ const thisRouter = require('./thisRouter')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.SERVER_PORT
 
+// Set our backend port to be either an environment variable or port 5000
+const portServer = process.env.SERVER_PORT || 5000
+
+// Set our frontend port to be either an environment variable or port 5000
+const portClient = process.env.REACT_APP_PORT || 3000
 
 // Helpful Link #1: https://create-react-app.dev/docs/proxying-api-requests-in-development/
 //    CRA webpage that references Helpful Link #2
 // Helpful Link #2: https://enable-cors.org/server_expressjs.html
 //    What I copy and pasted from
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", `http://localhost:${process.env.REACT_APP_PORT}`); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", `http://localhost:${portClient}`); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -23,6 +27,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/send-email', thisRouter)
 
-app.listen(port, () => {
-  console.log(`Express app listening on port ${port}`)
+app.listen(portServer, () => {
+  console.log(`Express app listening on port ${portServer}`)
 })
