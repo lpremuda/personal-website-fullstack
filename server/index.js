0ddd376e26e627f248a2d325a261c5f728b5fb9d
lpremuda <lucas.premuda@gmail.com>
2021-05-23 20:12:31 -0700

    Commented out Access-Control-Allow-Methods

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ if (!isDev && cluster.isMaster) {
   //    What I copy and pasted from
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+    // res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
