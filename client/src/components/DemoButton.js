3070e1834f42e0fa7abc50915f6887dd89c28a1d
lpremuda <lucas.premuda@gmail.com>
2021-07-04 10:27:19 -0700

    Refactored DemoButton.js

--- client/src/components/DemoButton.js
+++ client/src/components/DemoButton.js
@@ -1,17 +1,38 @@
 import React from 'react'
 
-import ButtonLink from './ButtonLink'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import MuiLink from '@material-ui/core/Link'
+
+const useStyles = makeStyles({
+  buttonLink: {
+    textDecoration: 'none',
+  }
+})
 
 export default function DemoButton({ href }) {
+  const classes = useStyles()
 
   return (
-    <ButtonLink
-      href={href}
-      variant="contained"
-      color="secondary"
-      size="large"
-    >
-      View Demo
-    </ButtonLink>
+    // <ButtonLink
+    //   href={href}
+    //   variant="contained"
+    //   color="secondary"
+    //   size="large"
+    // >
+    //   View Demo
+    // </ButtonLink>
+
+    // Eventually insert a Material UI popover
+    <MuiLink target="_blank" rel="noreferrer" href={href} className={classes.buttonLink} >
+      <Button
+        href={href}
+        variant="contained"
+        color="secondary"
+        size="large"
+      >
+        View Demo
+      </Button>
+    </MuiLink>
   )
 } 
